2451e7adb760748731d258a5b5f2004d781a8c54
jonytellodev <jonathantello.dev@gmail.com>
2023-07-12 21:02:24 -0300

    Agregamos un state numero al componente Contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 const Contador = () => {
+    //logica
+    const[numero, setNumero] = useState(0);
+    
     return (
-        <section>
-            <h3> Contador </h3>
-            <h3>10</h3>
-            <Button variant="primary">+1</Button>
-        </section>
+      <section>
+        <h3> Contador </h3>
+        <h3>{numero}</h3>
+        <Button variant="primary" onClick={() => setNumero(numero + 1)}>
+          +1
+        </Button>
+      </section>
     );
 };
 
